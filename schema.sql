d9a8f8d8b15f7110306c9a7d830989d5817cd151
Brandon Lehmann <brandonlehmann@gmail.com>
2019-08-31 09:38:06 -0400

    Added support for storing the reported difficulty, hashrate, and transaction pool size during the node polling event

--- schema.sql
+++ schema.sql
@@ -1,32 +1,30 @@
-CREATE TABLE IF NOT EXISTS `nodes`
-  (
-     `id`       VARCHAR(64) NOT NULL,
-     `name`     VARCHAR(255) NOT NULL,
-     `hostname` VARCHAR(255) NOT NULL,
-     `port`     INT(11) NOT NULL DEFAULT 11898,
-     `ssl`      INT(11) NOT NULL DEFAULT 0,
-     `cache`    INT(11) NOT NULL DEFAULT 0,
-     PRIMARY KEY (`id`),
-     KEY `ssl` (`ssl`),
-     KEY `cache` (`cache`)
-  )
-engine=innodb
-DEFAULT charset=utf8;
+CREATE TABLE IF NOT EXISTS `nodes` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `hostname` varchar(255) NOT NULL,
+  `port` int(11) NOT NULL DEFAULT 11898,
+  `ssl` int(11) NOT NULL DEFAULT 0,
+  `cache` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `ssl` (`ssl`),
+  KEY `cache` (`cache`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `node_polling`
-  (
-     `id`             VARCHAR(64) NOT NULL,
-     `timestamp`      BIGINT(1) UNSIGNED NOT NULL,
-     `status`         INT(11) NOT NULL DEFAULT 0,
-     `feeaddress`     VARCHAR(255) DEFAULT NULL,
-     `feeamount`      BIGINT(20) NOT NULL DEFAULT 0,
-     `height`         BIGINT(20) NOT NULL DEFAULT 0,
-     `version`        VARCHAR(20) NOT NULL DEFAULT '0.0.0',
-     `connectionsin`  INT(11) NOT NULL DEFAULT 0,
-     `connectionsout` INT(11) NOT NULL DEFAULT 0,
-     PRIMARY KEY (`id`, `timestamp`),
-     KEY `status` (`status`),
-     KEY `feeamount` (`feeamount`)
-  )
-engine=innodb
-DEFAULT charset=utf8;
+CREATE TABLE IF NOT EXISTS `node_polling` (
+  `id` varchar(64) NOT NULL,
+  `timestamp` bigint(1) unsigned NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `feeAddress` varchar(255) DEFAULT NULL,
+  `feeAmount` bigint(20) NOT NULL DEFAULT 0,
+  `height` bigint(20) NOT NULL DEFAULT 0,
+  `version` varchar(20) NOT NULL DEFAULT '0.0.0',
+  `connectionsIn` int(11) NOT NULL DEFAULT 0,
+  `connectionsOut` int(11) NOT NULL DEFAULT 0,
+  `difficulty` bigint(20) NOT NULL DEFAULT 0,
+  `hashrate` bigint(20) NOT NULL DEFAULT 0,
+  `txPoolSize` bigint(20) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`,`timestamp`),
+  KEY `status` (`status`),
+  KEY `feeAmount` (`feeAmount`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPRESSED;
+
